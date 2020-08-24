26faa710d6b34e5dc6bd5bef829c3be2a2598bc9
haoruigang <1373939387@qq.com>
2020-08-24 16:49:52 +0800

    kotlin 基础库

---
+++ app/src/main/java/com/gang/library/MyApplication.kt
@@ -0,0 +1,19 @@
+package com.gang.library
+
+
+class MyApplication : BaseApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        init()
+    }
+
+    private fun init() { //
+
+    }
+
+    companion object {
+
+    }
+}
---
+++ app/src/main/java/com/gang/library/ui/adapter/HomeMenuAdapter.kt
@@ -0,0 +1,37 @@
+package com.gang.library.ui.adapter
+
+import android.content.Context
+import com.gang.library.common.view.xrecyclerview.xrecycleradapter.XrecyclerAdapter
+import com.gang.library.common.view.xrecyclerview.xrecycleradapter.XrecyclerViewHolder
+import com.gang.library.ui.bean.HomeIcon
+import kotlinx.android.synthetic.main.item_home_menu.view.*
+
+/**
+ *
+ * @ProjectName:    gang
+ * @Package:        com.gang.app.ui.adapter
+ * @ClassName:      HomeMenuAdapter
+ * @Description:     java类作用描述
+ * @Author:         haoruigang
+ * @CreateDate:     2020/8/5 16:00
+ * @UpdateUser:     更新者：
+ * @UpdateDate:     2020/8/5 16:00
+ * @UpdateRemark:   更新说明：
+ * @Version:        1.0
+ */
+class HomeMenuAdapter(
+    datas: MutableList<*>, context: Context, override val layoutResId: Int
+) :
+    XrecyclerAdapter(datas, context) {
+
+    var homeIcon: HomeIcon? = null
+
+    override fun convert(holder: XrecyclerViewHolder, position: Int, context: Context) {
+        holder.view.apply {
+            homeIcon = datas[position] as HomeIcon
+
+            homeIcon?.icon?.toInt()?.let { iv_icon?.setImageResource(it) }
+            tv_name?.text = homeIcon?.name
+        }
+    }
+}
